c4d5c3b55bf9195d0ba2926a3a811611a2528410
sylviaYou92 <shiyangsylviayou@gmail.com>
2018-09-12 14:29:46 +0800

    fix

--- spider_news_all/pipelines.py
+++ spider_news_all/pipelines.py
@@ -27,6 +27,7 @@ class SpiderNewsAllPipeline(object):
     cursor.execute('SET CHARACTER SET utf8;')
     cursor.execute('SET character_set_connection=utf8;')
 
+    INSERT_NEWS_RECORD = ("INSERT INTO news_record (linkmd5id) VALUES (%s)")
     INSERT_TYPE_ID = ("INSERT INTO dede_arctype (typename,typedir,tempindex,templist,temparticle,namerule,namerule2,isdefault) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)")
     INSERT_ARCTINY = ("INSERT INTO dede_arctiny (typeid,typeid2,mid) VALUES (%s,%s,%s)")
     INSERT_ADDONARTICLE = ("INSERT INTO dede_addonarticle (aid,typeid,body,redirecturl) VALUES (%s,%s,%s,%s)")
@@ -39,23 +40,24 @@ class SpiderNewsAllPipeline(object):
         self.cursor.execute("select * from news_record where linkmd5id = %s", (linkmd5id, ))
         ret = self.cursor.fetchone()
 
-	tempindex = '{style}/index_article.htm'
-	templist = '{style}/list_article.htm'
-	temparticle = '{style}/article_article.htm'
-	namerule = '{typedir}/{Y}/{M}{D}/{aid}.html'
-	namerule2 = '{typedir}/list_{tid}_{page}.html'
-	isdefault = '-1'
+	    tempindex = '{style}/index_article.htm'
+	    templist = '{style}/list_article.htm'
+	    temparticle = '{style}/article_article.htm'
+	    namerule = '{typedir}/{Y}/{M}{D}/{aid}.html'
+	    namerule2 = '{typedir}/list_{tid}_{page}.html'
+	    isdefault = '-1'
 
         if ret:
             pass
         else:
+            self.cursor.execute(self.INSERT_NEWS_RECORD,linkmd5id)
             # if type exists
             self.cursor.execute("select id from dede_arctype where typename = %s",(_type,))
             typeid = self.cursor.fetchone()
             if typeid:
                 pass
             else:
-		typedir = "{cmspath}/a/"+self._get_linkmd5id(_type)
+		        typedir = "{cmspath}/a/"+self._get_linkmd5id(_type)
                 self.cursor.execute(self.INSERT_TYPE_ID, (_type,typedir,tempindex,templist,temparticle,namerule,namerule2,isdefault))
                 self.cursor.execute("select max(id) from dede_arctype")
                 typeid = self.cursor.fetchone()
@@ -65,7 +67,7 @@ class SpiderNewsAllPipeline(object):
             if typeid2:
                 pass
             else:
-		typedir = "{cmspath}/a/"+self._get_linkmd5id(site)
+		        typedir = "{cmspath}/a/"+self._get_linkmd5id(site)
                 self.cursor.execute(self.INSERT_TYPE_ID, (site,typedir,tempindex,templist,temparticle,namerule,namerule2,isdefault))
                 self.cursor.execute("select max(id) from dede_arctype")
                 typeid2 = self.cursor.fetchone()
@@ -78,9 +80,7 @@ class SpiderNewsAllPipeline(object):
              
             self.cursor.execute(self.INSERT_ADDONARTICLE, (articleid, typeid, markdown, url))
 
-            #self.cursor.execute(self.INSERT_ARCHIVES, (articleid, typeid, typeid2, title, site, day, keywords,'0','1'))
-	    #INSERT_ARCHIVES = ("INSERT INTO dede_archives ( id, typeid, typeid2, sortrank, flag, ismake, channel, arcrank, click, money, title, shorttitle, color, writer, source, litpic, pubdate, senddate, mid, keywords, lastpost, scores, goodpost, badpost, voteid, notpost, description, filename, dutyadmin, tackid, mtype, weight) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)")
-	    self.cursor.execute(self.INSERT_ARCHIVES,(articleid, typeid, 1, day, "", "-1", "1","0", "0", "0", title, "", "", "", site, "", day, day, "0", keywords, "0", "0", "0", "0", "0", "0", "", "", "0", "0", "0", "0"))
+	        self.cursor.execute(self.INSERT_ARCHIVES,(articleid, typeid, 1, day, "", "-1", "1","0", "0", "0", title, "", "", "", site, "", day, day, "0", keywords, "0", "0", "0", "0", "0", "0", "", "", "0", "0", "0", "0"))
             
 
             """try:
